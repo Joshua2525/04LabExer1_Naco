6bbb3d4ea56a5c6de559427afe471a9a05b8b198
Joshua2525 <69311400+Joshua2525@users.noreply.github.com>
2022-10-28 12:57:14 +0800

    Add files via upload

---
+++ BasicThread.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Naco_Threads
+{
+    public partial class BasicThread : Form
+    {
+
+        private Thread ThreadA = new Thread(MyThreadClass.Thread1);
+        private Thread ThreadB = new Thread(MyThreadClass.Thread1);
+
+        public BasicThread()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadA.Name = "Thread A";
+            ThreadB.Name = "Thread B";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            label1.Text = "- End of Thread -";
+
+        }
+    }
+}
+    
+
---
+++ MyThreadClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Naco_Threads
+{
+
+    class MyThreadClass
+    {
+        
+          public static void Thread1()
+        {
+            for (int th = 0; th <= 5; th++)
+            {
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + th);
+                Thread.Sleep(5000);
+
+            }
+
+        }
+
+    }
+}
+
+
